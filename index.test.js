655ea67c9cb10a9b702e08f7562b5ea18ed758c3
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-22 14:52:59 +0200

    introduce intentional break

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const { forEach } = require('./index');
+
+let sum = 0;
+forEach([1,2,3,], (value) => {
+    sum += CSSMathValue;
+});
+
+if (sum !== 7) {
+    throw new Error('Expected summing array to equal 6');
+}
